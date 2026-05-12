d3fe221a994e037a67d9ad14d7cab40772a1326d
kairichelsea <kairichelsea@gmail.com>
2026-05-11 18:58:53 -0700

    authorization page basic set up

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -2,12 +2,32 @@ import "./Login.css";
 
 function Login() {
   return (
-    <div className="login-page">
-      <div className="login-card">
-        <h1>Login</h1>
-        <input type="email" placeholder="Email" />
-        <input type="password" placeholder="Password" />
-        <button>Log In</button>
+    <div className="auth-page">
+      <div className="auth-container">
+        
+        {/*Sign Up*/}
+        <div className="sign-log sign-in-card">
+          <h1>Sign Up</h1>
+          <p>Create a new account</p>
+
+          <input type="text" placeholder="Username" />
+          <input type="email" placeholder="Email" />
+          <input type="password" placeholder="Password" />
+
+          <button>Sign Up</button>
+        </div>
+
+        {/*Log In*/}
+        <div className="sign-log log-in-card">
+          <h1>Log In</h1>
+          <p>Welcome back!</p>
+
+          <input type="email" placeholder="Email" />
+          <input type="password" placeholder="Password" />
+
+          <button>Log In</button>
+        </div>
+
       </div>
     </div>
   );
