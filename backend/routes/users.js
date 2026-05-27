c852bdea7b34c38e1d05adeb5399f9dbe4f073bc
Ethan Jin <ethjin8@gmail.com>
2026-05-26 22:43:28 -0700

    Added endpoint to delete streaming services + refactored into new streaming.js file

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -3,7 +3,6 @@ const bcrypt = require('bcrypt');
 const pool = require('../database.js');
 const router = express.Router();
 
-const { authenticateToken } = require('../middleware/tokens.js');
 const { getAllUsers, findUser, addUser } = require('../models/users.js');
 
 
@@ -33,49 +32,5 @@ router.post('/users', async (req, res) => {
   }
 })
 
-// Endpoint to get user's streaming services
-router.get('/streaming', authenticateToken, async (req, res) => {
-  try {
-    const uid = req.user.id;
-    const [result] = await pool.query(`
-      SELECT * FROM streaming_services WHERE user_id = ?
-    `, [uid]);
-    res.status(200).json(result);
-  } catch (err) {
-    return res.status(500).json({ error: err.message });
-  }
-})
-
-router.delete('/streaming', authenticateToken, async (req, res) => {
-  try {
-    const uid = req.user.id;
-    const streamingService = req.body.streaming;
-
-    const [result] = await pool.query(`
-      DELETE FROM streaming_services WHERE user_id = ? AND streaming_service = ?
-    `, [uid, streamingService]);
-
-    res.json(result);
-  } catch (err) {
-    return res.status(500).json({ error: err.message });
-  }
-})
-
-// Endpoint to add streaming services to user profile
-router.post('/streaming', authenticateToken, async (req, res) => {
-  try {
-    const uid = req.user.id;
-    const streamingService = req.body.streaming;
-
-    const [result] = await pool.query(`
-      INSERT INTO streaming_services (user_id, streaming_service)
-      VALUES(?, ?)
-    `, [uid, streamingService]);
-
-    res.json(result);
-  } catch (err) {
-    return res.status(500).json({ error: err.message });
-  }
-})
 
 module.exports = router;
