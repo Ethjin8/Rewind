995297688fa42249247907e8b6fdaef0f33896b2
kairichelsea <kairichelsea@gmail.com>
2026-05-29 02:05:50 -0700

    Removed accidentally created file

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -2,6 +2,15 @@ export default function Profile() {
   return (
     <div className="p-6">
       <h1>Profile</h1>
+      <button
+        className="account-avatar-btn"
+      >
+        <img
+          src="/profile-blank.png"
+          alt=""
+          className="account-avatar"
+        />
+      </button>
     </div>
   );
 }
