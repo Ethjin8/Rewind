fdffb6afdca3c60f284a2943a8a9c0d6e01925c9
kairichelsea <kairichelsea@gmail.com>
2026-05-30 07:42:48 -0700

    Fully integrated checkAvail.js.

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -4,7 +4,7 @@ import './Home.css';
 import '../pages/Auth.css';
 import MovieCarousel from '../components/MovieCarousel';
 import { authFetch } from '../lib/authFetch';
-
+import { hasSelectedStreamingService } from '../lib/checkAvailability';
 const INITIAL_BACKLOG = [];
 
 function getPosterSrc(path) {
@@ -36,7 +36,6 @@ export default function Home() {
   const [error, setError] = useState(null);
   //Gets the streaming service selected by the user from profile,
   // returns null if no streaming service is selected.
-  const selectedServices = JSON.parse(localStorage.getItem("selectedServices")) || [];
 
   useEffect(() => {
     let mounted = true;
@@ -72,27 +71,27 @@ export default function Home() {
     }
   }, [location.pathname, loginMsg, navigate]);
 
-  function normalizeProviderName(name) {
-    if (name.includes('Max')) return 'HBO Max';
-    if (name.includes('Amazon')) return 'Prime Video';
-    if (name.includes('Crunchyroll')) return 'Crunchyroll';
-    if (name.includes('Peacock')) return 'Peacock';
-    if (name.includes('Paramount')) return 'Paramount+';
-    if (name.includes('Disney')) return 'Disney+';
-    if (name.includes('Netflix')) return 'Netflix';
-    if (name.includes('Apple')) return 'Apple TV';
-}
-  // Returns a bool indicating whether the given movie has a streaming provider that
-  //  matches the user's selected streaming services.
-  function hasSelectedStreamingService(movie) {
-    const providers =
-      movie["watch/providers"]?.results?.US?.flatrate || [];
-  // .some iterates through the providers and returns true if 
-  // *any provider's name is included in the user's selected services.
-    return providers.some((provider) =>
-      selectedServices.includes( normalizeProviderName(provider.provider_name) )
-    );
-  }
+  // function normalizeProviderName(name) {
+  //   if (name.includes('Max')) return 'HBO Max';
+  //   if (name.includes('Amazon')) return 'Prime Video';
+  //   if (name.includes('Crunchyroll')) return 'Crunchyroll';
+  //   if (name.includes('Peacock')) return 'Peacock';
+  //   if (name.includes('Paramount')) return 'Paramount+';
+  //   if (name.includes('Disney')) return 'Disney+';
+  //   if (name.includes('Netflix')) return 'Netflix';
+  //   if (name.includes('Apple')) return 'Apple TV';
+  //}
+  // // Returns a bool indicating whether the given movie has a streaming provider that
+  // //  matches the user's selected streaming services.
+  // function hasSelectedStreamingService(movie) {
+  //   const providers =
+  //     movie["watch/providers"]?.results?.US?.flatrate || [];
+  // // .some iterates through the providers and returns true if 
+  // // *any provider's name is included in the user's selected services.
+  //   return providers.some((provider) =>
+  //     selectedServices.includes( normalizeProviderName(provider.provider_name) )
+  //   );
+  // }
 
   const availableBacklog = backlogItems.filter((movie) => 
       !movie.removed && hasSelectedStreamingService(movie));
